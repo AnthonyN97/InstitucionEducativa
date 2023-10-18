e9c059bbc206db87b5857eaffa019ded202718dc
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-10-18 17:38:04 -0500

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Header from './components/header/Header';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <>hola</>,
+  },
+  {
+    path: '/login',
+    element: <>login</>,
+  },
+  {
+    path: '/test',
+    element: <>test</>,
+  }
+])
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Header/>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
