2d52a3b75d1323e7bd714905d9f9a3d9c627e26a
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-10-23 15:02:27 -0500

    cambios den presentacion (falta mejorar imagen o una con mejor resolucion)

--- src/components/MisionVision/Mision.tsx
+++ src/components/MisionVision/Mision.tsx
@@ -4,9 +4,8 @@ const Mision: React.FC = () => (
             <div className="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 dark:bg-gray-800">
                 <div className="flex-shrink-0">
                     <div className="flex items-center justify-center w-12 h-12 mx-auto text-white bg-indigo-500 rounded-md">
-                        <svg width="20" height="20" fill="currentColor" className="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
                         </svg>
                     </div>
                 </div>
@@ -14,19 +13,19 @@ const Mision: React.FC = () => (
                     Mision
                 </h3>
                 <p className="py-4 text-gray-500 text-md dark:text-gray-300">
-                    Nuestra mision es Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit 
-                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant 
-                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo 
-                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate, 
+                    Nuestra mision es Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit
+                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant
+                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo
+                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate,
                     habitant pretium urna hac penatibus platea semper ad.
                 </p>
             </div>
             <div className="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 sm:mt-16 md:mt-20 lg:mt-24 dark:bg-gray-800">
                 <div className="flex-shrink-0">
                     <div className="flex items-center justify-center w-12 h-12 mx-auto text-white bg-indigo-500 rounded-md">
-                        <svg width="20" height="20" fill="currentColor" className="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                         </svg>
                     </div>
                 </div>
@@ -34,30 +33,30 @@ const Mision: React.FC = () => (
                     Visión
                 </h3>
                 <p className="py-4 text-gray-500 text-md dark:text-gray-300">
-                    Nuestra visión es Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit 
-                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant 
-                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo 
-                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate, 
+                    Nuestra visión es Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit
+                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant
+                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo
+                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate,
                     habitant pretium urna hac penatibus platea semper ad.
                 </p>
             </div>
             <div className="w-full px-4 py-4 mt-6 bg-white rounded-lg shadow-lg sm:w-1/2 md:w-1/2 lg:w-1/4 dark:bg-gray-800">
                 <div className="flex-shrink-0">
                     <div className="flex items-center justify-center w-12 h-12 mx-auto text-white bg-indigo-500 rounded-md">
-                        <svg width="20" height="20" fill="currentColor" className="w-6 h-6" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M491 1536l91-91-235-235-91 91v107h128v128h107zm523-928q0-22-22-22-10 0-17 7l-542 542q-7 7-7 17 0 22 22 22 10 0 17-7l542-542q7-7 7-17zm-54-192l416 416-832 832h-416v-416zm683 96q0 53-37 90l-166 166-416-416 166-165q36-38 90-38 53 0 91 38l235 234q37 39 37 91z">
-                            </path>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
                         </svg>
+
                     </div>
                 </div>
                 <h3 className="py-4 text-2xl font-semibold text-gray-700 sm:text-xl dark:text-white">
                     Porque nosotros
                 </h3>
                 <p className="py-4 text-gray-500 text-md dark:text-gray-300">
-                    Eligenos por Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit 
-                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant 
-                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo 
-                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate, 
+                    Eligenos por Lorem ipsum dolor sit amet consectetur adipiscing elit lectus, dictumst litora dui per hendrerit
+                    class sociis. Auctor non senectus orci fusce duis congue sem tellus scelerisque, blandit potenti quisque habitant
+                    faucibus lectus vel litora sapien accumsan, neque posuere nunc metus pharetra varius dui nisi. Morbi et commodo
+                    egestas donec fames velit massa molestie fringilla, ultricies porta nam aenean eu dui nullam lectus vulputate,
                     habitant pretium urna hac penatibus platea semper ad.
                 </p>
             </div>
--- src/components/Presentacion/Presentacion.tsx
+++ src/components/Presentacion/Presentacion.tsx
@@ -1,18 +1,22 @@
-import logo from '../../assets/logo.jpg';
+import student from '../../assets/student.jpg';
 
 const Presentacion: React.FC = () => {
-  return (
-    <div
-      className="bg-cover bg-center bg-no-repeat w-full h-64 md:h-96"
-      style={{ backgroundImage: `url(${logo})` }} // Usamos ${logo} para insertar la URL de la imagen
-    >
-      <div className="flex justify-end items-center h-full">
-        <div className="text-white text-2xl md:text-4xl font-semibold p-4 md:p-8">
-          Tu texto a la derecha
+    return (
+        <div
+            className="bg-cover bg-center bg-no-repeat w-full h-64 md:h-96 relative" // Agregamos la clase `relative` para posicionar elementos hijos
+            style={{ backgroundImage: `url(${student})` }}
+        >
+            <div className="absolute inset-0 bg-black opacity-50"></div>
+            <div className="flex flex-col justify-end items-end h-full relative">
+                <h1 className="text-white text-4xl md:text-6xl font-semibold p-4 md:p-8">
+                    Escuela Corpaidos 
+                </h1>
+                <p className="text-white text-2xl md:text-4xl p-4 md:p-8">
+                    Dedicados a la educación desde 19xx
+                </p>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Presentacion;
