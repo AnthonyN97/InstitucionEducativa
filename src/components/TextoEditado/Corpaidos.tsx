cbc7c2f738c0f64a16b0eb77ff8400ffac4cbf9b
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-10-24 13:43:04 -0500

    letras con tipografica para corpaidos hecho, modificacion en presentacion hecho

---
+++ src/components/TextoEditado/Corpaidos.tsx
@@ -0,0 +1,13 @@
+import './Corpaidos.css';
+
+const Corpaidos: React.FC = () => {
+    return (
+        <>
+            <p className='colegio'>Colegio</p>
+            <p className='outline-title'>Corpaidos</p>
+        </>
+
+    );
+};
+
+export default Corpaidos;
