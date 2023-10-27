52ae3935bf7e6f6cf1ed54c028c23366b56616a3
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-10-27 13:23:16 -0500

    agregado de espaciado a los principios

--- src/components/Principios/PrincipiosRectores.tsx
+++ src/components/Principios/PrincipiosRectores.tsx
@@ -37,15 +37,17 @@ const PrincipiosRectores: React.FC = () => {
            <p className="pt-4 text-center text-2xl font-semibold mb-4 sm:pt-6">Principios Rectores:</p>
             <div className="grid mb-8 p-3 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
                 {datosEducacion.map((item, index) => (
-                    <figure
+                    <div className="content p-2">
+                        <figure
                         key={index}
-                        className="flex flex-col items-center justify-center p-3 text-center bg-white border-b border-gray-200 rounded-lg md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700"
+                        className="flex flex-col h-full items-center justify-center p-3 text-center bg-white border-b border-gray-200 rounded-lg md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700"
                     >
                         <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 dark:text-gray-400">
                             <h3 className="text-lg font-semibold text-gray-900 dark:text-white">{item.nombre}</h3>
                             <p className="my-4">{item.descripcion}</p>
                         </blockquote>
                     </figure>
+                    </div>
                 ))}
             </div>
         </>
