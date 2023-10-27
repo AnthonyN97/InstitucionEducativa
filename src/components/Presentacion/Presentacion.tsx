1c6e7fa93333a32c8bf03acc1f6b6f9ac6610ec5
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-10-27 11:29:03 -0500

    agregado de estilos de presentacion sobre nosotros y cambios en misionvision para que sea responsivo

--- src/components/Presentacion/Presentacion.tsx
+++ src/components/Presentacion/Presentacion.tsx
@@ -1,37 +1,50 @@
 const Presentacion: React.FC = () => {
-    return(
-        <>
-        Presentación
-
-ESTIMADOS PADRES Y MADRES DE ALUMNOS/AS
-
-Una de las características de un buen gobierno, sea en el hogar, en la empresa, en la oficina y particularmente en el colegio es sin duda la planificación.
-
-El colegio Divino Niño CORPAIDOS asume la planificación como un medio, como un trazo, el derrotero que nos proponemos alcanzar juntos: Familia – colegio, durante un periodo de tiempo relativamente corto: el año 2014.
-
-Por consiguiente, al puerto al que queremos llegar a partir de lo que planeamos lo que somos: una comunidad educativa en crecimiento y convencida, que sí es posible impartir la mejor educación escolar cuando padres y profesores trabajan juntos, con un solo objetivo: Educar para vivir y trascender.
-
-Para plasmar la ruta por la que queremos lograr nuestros objetivos les presentamos la AGENDA EDUCATIVA 2014.
-
-Este instrumento refleja el ANUARIO, EL CALENDARIO, LA ORGANIZACIÓN ESCOLAR y naturalmente la agenda diaria de los acontecimientos educativos que deben ocurrir.
-
-La agenda es pues como el testimonio vivo, real y objetivo de cada día, de los logros, los éxitos pero también las limitaciones, las dificultades de nuestros niños y niñas.
-
-De modo que su registro es obligatorio para nuestros docentes e indispensable para los padres y madres. Sin control mutuo, nuestra planificación puede ser una mera formalidad y eso es lo que no queremos.
-
-Por esta razón les pedimos, revisarla cada día reflejar sus comentarios, firmarla, así lograremos juntos hacer de sus hijos/as nuestros alumnos los mejores.
-
-Es obvio que la agenda debe portarse obligatoriamente todos los días y esta debe ser forrada.
-
-Estamos seguros que con ella los padres y madres encontrarán la información que necesitan y merecen conocer de la comunidad de la que forman parte.
-
-Con afecto.
-
-
-Prof. Rosa María Huaco de la Cruz
-DIRECTORA
-
-        </>
+    return (
+        <div className="container flex-col mx-auto p-4 sm:p-6 md:p-8 text-justify">
+            <h1 className="text-3xl font-bold mb-4">
+                Presentación
+            </h1>
+            <p className="text-lg text-gray-700 uppercase py-5">
+                Estimados padres y madres de alumnos/as
+            </p>
+            <p className="text-base leading-7 py-1">
+                Una de las características de un buen gobierno, sea en el hogar, en la empresa, en la oficina y particularmente en el colegio es sin duda la planificación.
+            </p>
+            <p className="text-base leading-7 py-1">
+                El colegio Divino Niño CORPAIDOS asume la planificación como un medio, como un trazo, el derrotero que nos proponemos alcanzar juntos: Familia – colegio, durante un periodo de tiempo relativamente corto: el año 2014.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Por consiguiente, al puerto al que queremos llegar a partir de lo que planeamos lo que somos: una comunidad educativa en crecimiento y convencida, que sí es posible impartir la mejor educación escolar cuando padres y profesores trabajan juntos, con un solo objetivo: Educar para vivir y trascender.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Para plasmar la ruta por la que queremos lograr nuestros objetivos les presentamos la AGENDA EDUCATIVA 2014.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Este instrumento refleja el ANUARIO, EL CALENDARIO, LA ORGANIZACIÓN ESCOLAR y naturalmente la agenda diaria de los acontecimientos educativos que deben ocurrir.
+            </p>
+            <p className="text-base leading-7 py-1">
+                La agenda es pues como el testimonio vivo, real y objetivo de cada día, de los logros, los éxitos pero también las limitaciones, las dificultades de nuestros niños y niñas.
+            </p>
+            <p className="text-base leading-7 py-1">
+                De modo que su registro es obligatorio para nuestros docentes e indispensable para los padres y madres. Sin control mutuo, nuestra planificación puede ser una mera formalidad y eso es lo que no queremos.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Por esta razón les pedimos, revisarla cada día reflejar sus comentarios, firmarla, así lograremos juntos hacer de sus hijos/as nuestros alumnos los mejores.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Es obvio que la agenda debe portarse obligatoriamente todos los días y esta debe ser forrada.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Estamos seguros que con ella los padres y madres encontrarán la información que necesitan y merecen conocer de la comunidad de la que forman parte.
+            </p>
+            <p className="text-base leading-7 py-1">
+                Con afecto.
+            </p>
+            <p className="text-sm text-gray-500 py-5">
+                Prof. Rosa María Huaco de la Cruz
+                DIRECTORA
+            </p>
+        </div>
     );
 };
 
