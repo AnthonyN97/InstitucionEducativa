6335feb7e949a25e043bd26316783a5027bc4339
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-11-03 11:34:45 -0500

    agregado de estilos e imagenes para la parte de principios

--- src/components/Principios/PrincipiosRectores.tsx
+++ src/components/Principios/PrincipiosRectores.tsx
@@ -1,30 +1,45 @@
+import Test from "./test";
+import desarrolloHumano from "../../assets/desarrolloHumano.jpg";
+import etica from "../../assets/etica.jpg";
+import intercultural from "../../assets/intercultural.jpg";
+import construccionAprendizajes from "../../assets/construccionAprendizajes.jpg";
+import pertinenciaVocacional from "../../assets/pertinenciaVocacional.jpg";
+import propedeutica from "../../assets/propedeutica.jpg";
+
 interface Principios {
     nombre: string,
     descripcion: string,
+    imagen: string
 }
 
 const datosEducacion:Principios[] = [
     {
+        imagen: desarrolloHumano,
         nombre: "Desarrollo Humano",
         descripcion: "La educación que propugnamos se basa en la promoción integral de la persona, en el respeto de su singularidad, en el proceso evolutivo propio y en la atención a las necesidades individuales de sus educandos."
     },
     {
+        imagen: etica,
         nombre: "Ética",
         descripcion: "En tanto la educación fomenta los valores, la moral y la conducta ética aceptable, solidaridad, justa y de pleno respeto a la vida y la libertad."
     },
     {
+        imagen: intercultural,
         nombre: "Intercultural",
         descripcion: "Promotora de la relación intercultural y dialógica entre los diversos segmentos de la población escolar arequipeña."
     },
     {
+        imagen: construccionAprendizajes,
         nombre: "Construcción de los Aprendizajes",
         descripcion: "Nuestra concepción educativa es psico-pedagógica y concibe el aprendizaje como un proceso a la vez interno como en la relación con el medio."
     },
     {
+        imagen: pertinenciaVocacional,
         nombre: "Pertinencia Vocacional",
         descripcion: "Que busca descubrir todas las capacidades de los alumnos y alumnas, afianzando sus potencialidades para llegar a una decisión madura, pertinente en cuanto a su elección profesional sea humanístico como tecnológico."
     },
     {
+        imagen: propedeutica,
         nombre: "Propedéutica",
         descripcion: "Principio orientado a formar y preparar a los infantes para iniciar una escolarización reglada sin traumas ni dificultades, que se extiende a la primaria para acceder con éxito a los estudios secundarios superiores."
     }
@@ -33,11 +48,12 @@ const datosEducacion:Principios[] = [
 
 const PrincipiosRectores: React.FC = () => {
     return (
-        <>
+        <div className="md:mx-10">
            <p className="pt-4 text-center text-2xl font-semibold mb-4 sm:pt-6">Principios Rectores:</p>
-            <div className="grid mb-8 p-3 rounded-lg shadow-sm dark:border-gray-700 md:mb-12 md:grid-cols-2">
+            <div className="grid justify-center rounded-lg shadow-sm dark:border-gray-700 md:w-full md:grid-cols-2">
                 {datosEducacion.map((item, index) => (
-                    <div className="content p-2">
+                    <Test Principios={item}/>
+                    /*<div className="content p-2">
                         <figure
                         key={index}
                         className="flex flex-col h-full items-center justify-center p-3 text-center bg-white border-b border-gray-200 rounded-lg md:rounded-tl-lg md:border-r dark:bg-gray-800 dark:border-gray-700"
@@ -47,10 +63,10 @@ const PrincipiosRectores: React.FC = () => {
                             <p className="my-4">{item.descripcion}</p>
                         </blockquote>
                     </figure>
-                    </div>
+                    </div>*/
                 ))}
             </div>
-        </>
+        </div>
     );
 };
 
---
+++ src/components/Principios/test.tsx
@@ -0,0 +1,25 @@
+interface Principios {
+    nombre: string,
+    descripcion: string,
+    imagen: string
+}
+
+const Test: React.FC<{ Principios: Principios }> = ({ Principios }) => {
+    return (
+        <div className="flex flex-col m-5 rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:flex-row">
+            {Principios.imagen !== "/" ? (
+                <img className="h-86 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={Principios.imagen} alt="" />
+            ) : <></>}
+            <div className="flex flex-col items-start justify-start p-6">
+                <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+                    {Principios.nombre}
+                </h5>
+                <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+                    {Principios.descripcion}
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Test;
