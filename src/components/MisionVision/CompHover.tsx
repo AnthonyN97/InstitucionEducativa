2ac3c0c15c0eb39f0703a28ac8f7e078faa33fd5
Anthony Nova <anthony.nova@tecsup.edu.pe>
2023-11-16 11:49:13 -0500

    antes de los cambios en mision y vision

--- src/components/MisionVision/CompHover.tsx
+++ src/components/MisionVision/CompHover.tsx
@@ -1,37 +1,42 @@
-import { useState } from "react"
+import { useState } from "react";
 
 interface Images {
-    url: string,
-    text: string
+  url: string;
+  text: string;
 }
 
 interface CompHoverProps {
-    images: Images[]
+  images: Images[];
 }
 
 const CompHover: React.FC<CompHoverProps> = ({ images }) => {
-    const [hoveredImage, setHoveredImage] = useState<number | null>(null);
+  const [hoveredImage, setHoveredImage] = useState<number | null>(null);
 
-    return (
-        <div className="flex justify-center m-5 flex-col rounded-lg">
-            {images.map((image, index) => (
-                <div
-                    key={index}
-                    className="flex flex-col m-2 rounded-lg dark:bg-blue-dark bg-corn-light sm:flex-row "
-                    onMouseOver={() => setHoveredImage(index)}
-                    onMouseOut={() => setHoveredImage(null)}
-                >
-                    <img src={image.url} alt="" className="w-1/4 h-1/4 object-cover basis-1 mx-auto" />
-                    {hoveredImage === index && (
-                        <div className="ml-4 p-2 shadow-md basis-2/3 rounded-lg text-xl text-justify inline-block align-bottom dark:text-white">
-                            {image.text}
-                        </div>
-                    )}
-                </div>
-
-            ))}
+  return (
+    <div className="flex justify-center m-5 flex-col rounded-lg">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`flex flex-col m-2 rounded-lg dark:bg-blue-dark bg-corn-light sm:flex-row ${
+            hoveredImage === index ? "hovered" : ""
+          }`}
+          onMouseOver={() => setHoveredImage(index)}
+          onMouseOut={() => setHoveredImage(null)}
+        >
+          <img
+            src={image.url}
+            alt=""
+            className="w-1/4 h-1/4 object-cover basis-1 mx-auto"
+          />
+          {hoveredImage === index && (
+            <div className="ml-4 p-2 shadow-md basis-2/3 rounded-lg text-lg text-justify inline-block align-middle dark:text-white items-center">
+              {image.text}
+            </div>
+          )}
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default CompHover;
